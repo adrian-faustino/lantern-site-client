08af995f8f96fef360fdd6ec66d996a38920c117
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-05-26 16:49:46 -0400

    basic footer setup

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import './Footer.css'
 
 export default function Footer() {
   return (
-    <div>
-      Footer.js
-    </div>
+    <footer className="Footer__container">
+      
+      <section className="Footer__follow-container">
+        <h3 className="Footer__follow-headline">Follow us on:</h3>
+
+        <ul className="Footer__followBtns-container">
+          <li>Facebook</li>
+          <li>Twitter</li>
+          <li>Instagram</li>
+        </ul>
+      </section>
+
+      <hr />
+      <p>contact.adrianfaustino@gmail.com</p>
+      
+    </footer>
   )
 }
